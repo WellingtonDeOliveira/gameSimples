8720f987cf94df52fe8ffd1b62d18d824c209b36
Wellington de Oliveira <85302795+WellingtonDeOliveira@users.noreply.github.com>
2022-06-22 14:06:14 -0300

    v1

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const junp = () => {
+    mario.classList.add('junp');
+
+    setTimeout(() => {
+        mario.classList.remove('junp');
+    }, 500);
+}
+const loop = setInterval(() => {
+   
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const pontos = +document.querySelector('.pontos').innerHTML ;
+    const valorPontos = document.querySelector('.pontos') 
+    if(pipePosition <= 120 && pipePosition > 0  && marioPosition < 80){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = './midia/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+        clearInterval(loop);
+    }
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition == 180 ){
+        setTimeout(() => {
+            valorPontos.innerHTML = `${pontos+1}`;
+        }, 80); 
+    }
+}, 10);
+document.addEventListener('keydown', junp);     
